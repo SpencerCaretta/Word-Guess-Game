0f97f2019e309daf35f031cd2d8ca71e775d75d5
Spencer Caretta <spencer.caretta@gmail.com>
2018-10-05 15:03:57 -0500

    lots of javascript changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
-var Cartoons = ["Animaniacs",
+// Array of possible Cartoons the game will choose from
+var Cartoon = ["Animaniacs",
                 "Rugrats",
                 "Doug",
                 "Arthur",
@@ -8,23 +9,60 @@ var Cartoons = ["Animaniacs",
                 "Johnny Bravo",
                 "Rocket Power",
                 "CatDog"];
-var wins = 0;
-var wrongGuess = [];
+
+//Choosing a Cartoon Randomly
+var ranNum = Math.floor(Math.random() * Cartoon.length);
+var selectedCartoon = Cartoon[ranNum];
+console.log(selectedCartoon);
+var underScore = [];
+var rightLetter = [];
+var wrongLetter = [];
+
+//Manipulate the HTML
+var underScoreManip = document.getElementsByClassName('underScore');
+
+//Creating the correct number of underscores based on the word length
+var createUnderscore = () => {
+    for(var i = 0; i < selectedCartoon.length; i++) {
+        underScore.push('_');
+        
+    }
+    return underScore;
+}
+
+//Getting the guess from the player
+document.addEventListener ('keypress', (event) => {
+    var keycode = event.keyCode;
+    var keyword = String.fromCharCode(keycode);
+
+//Code to check if the letter the player selected is in the world
+    if(selectedCartoon.indexOf(keyword) > -1) {
+        //If it is push to the correct section and display in the correct underscore space
+        rightLetter.push(keyword);
+        underScore[selectedCartoon.indexOf(keyword)] = keyword;
+        underScoreManip[0].innerHTML = underScore.join('');
+        //replace underscore with correct letter, if all letters are correct, alert you win!
+        if(underScore.join('') == selectedCartoon) {
+            alert('You Win, Congratulations!')
+        }
+        else {
+            //Push wrong guesses to our wrong word array
+            wrongLetter.push(keyword);
+        }
+    } 
+
+});
+//Display number of incorrect guesses the player can still have
 var guessesLeft = 10;
-var userGuesses = [];
+//Display number of player wins
+var wins = 0;
 
-var Hangman = Cartoons[Math.floor(Math.random() * Cartoons.length)];
+//If user gets all letters correct, display You Win and add 1 win to the counter
 
-var StringLength = Hangman.length
-
-for (i = 0; i < Hangman.length; i++) {
-    document.write("_")
-}
+//If the user guesses == 0, display you Lose, try again, and reset the game
 
 
-document.getElementById("Hangman").innerHTML = 
 
-document.getElementById("letter").onkeyup = function () {
-    Cartoons.includes("letter");
-    if Cartoons.includes("letter")
-}
+
+
+
